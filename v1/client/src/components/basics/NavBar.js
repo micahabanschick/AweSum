cc7c779d7989f145d90275c06354d13a0b2fecd9
micahabanschick <micha.ban@gmail.com>
2021-04-12 17:45:44 -0400

    NavBar

---
+++ v1/client/src/components/basics/NavBar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+const NavBar = (props) => {
+
+  console.log(props)
+
+  const navStyle = {
+    color: 'rgb(190,255,90)',
+    textDecoration: 'none',
+    display: 'inline-block'
+  }
+
+  return (
+    <Navbar expand="lg" className="App-nav">
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link className="nav-item" style={navStyle} to='#'>Wecome</Link>
+          <Link className="nav-item" style={navStyle} to='#'>Signup/Signin</Link>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default NavBar
