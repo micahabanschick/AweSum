87962bf1c7683a47bca0ec45119da9b73f2cfbe3
micahabanschick <micha.ban@gmail.com>
2021-04-12 17:47:59 -0400

    QuizesContainer

---
+++ v1/client/src/containers/QuizesContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import TabsComponent from '../components/basics/TabsComponent'
+// import Container from 'react-bootstrap/Container'
+// import Header from '../components/Header'
+// import Main from '../components/Main'
+// import Footer from '../components/Footer'
+
+class QuizesContainer extends Component {
+    render() {
+        return (
+            <div className="App-quizes-container">
+                <TabsComponent/>
+            </div>
+        )
+    }
+}
+
+export default QuizesContainer
